b680e5a7d7c1ecd8588274c62a0bbcdaacf92484
XO-NANO Smartfoam <jake.merrell@xonano.com>
2022-02-01 12:38:16 -0700

    Add files via upload

---
+++ CalibrateXOnanoPressureSensor.ino
@@ -0,0 +1,125 @@
+/*
+Max Tree
+XO-NANO
+2/1/22
+
+This code computes the numbers needed for calibrating an XO-NANO pressure sensor.
+Open the Serial Monitor and follow the instructions after uploading this code
+onto your microcontroller.
+
+This code is designed using an Arduino Uno.
+
+For the connection diagram between an Arduino Uno and an XO-NANO pressure
+sensor, see https://github.com/XOnanoSmartfoam/Dev-Kit-Resources.
+
+
+INSTRUCTIONS
+1. Find a flat object to distribute weight of known weights
+2. Find a known weight (dumbell, weighed object)
+3. Enter the known weight's value into the knownLoad variable
+4. Enter the the value for the sensorSurfaceArea (2x2, 1x1, 0.5x.05)
+5. Upload this code onto your microcontroller
+6. Follow instructions that come to the Serial Monitor tool.
+
+*/
+
+/****** User Inputs Section ******/
+/* Arduino Settings */
+int sensorPin = A0;
+//for the PWM pin, see setup() function.
+/* XO-NANO Pressure Sensor Settings */
+float VrmsSampleLimit = 500;
+float cutoffFreq = 1.0;  //Hz.
+float sampPeriod = 0.093;  //s. This number is from the timestamp of the Arduino IDE Serial Monitor tool.
+float knownLoad = 6.12; //lb
+float sensorSurfaceArea = 1.0; //square inches
+/****** End User Inputs Section ******/
+
+
+/* Initialize Variables */
+float sensorValue = 0.0;
+float Vrms = 500.0;
+float Vrmsd_1 = 500.0; // "d_1 stands for delayed 1.
+float Vrmsf = 500.0;   // "f" stands for filtered.
+float VrmsSS = 0.0; // Steady state ADC output
+
+
+
+float calculate_Vrms()
+{
+  float V = 0;
+  for (int i = 0; i < VrmsSampleLimit; i++)
+  {
+    sensorValue =  analogRead(sensorPin);
+    V = V + sensorValue*sensorValue;    
+  }
+
+  V = V/VrmsSampleLimit;
+  V = sqrt(V);
+
+  return V;
+}
+
+float lpf(float fc, float Ts, float y, float yd_1, float yfd_1) // fc is cutoff freq (Hz), Ts is sampling period (s).
+{                                                               // y is raw data and yf is filtered data and the d terms are delayed terms.
+  return (fc*y + yfd_1/Ts)/(1/Ts+fc);                           //derived by converting lpf lapace domain into time domain and assuming yf_dot = (yf-yfd_1)/Ts.
+}
+
+void check_for_known_pressure(float F)
+{
+  Serial.print("Enter any character when ");
+  Serial.print(F);
+  Serial.println("lb is evenly distributed on the XO-NANO pressure sensor.");
+  while(Serial.available()==0);
+  delay(100);
+  while(Serial.available() > 0)Serial.read();
+}
+
+void record_ADC_output_known_pressure(float F)
+{
+  int filterWarmUp = 400;
+  for( int i = 0; i < filterWarmUp; i++)
+  {
+    Vrms = calculate_Vrms();
+    Vrmsf = lpf(cutoffFreq, sampPeriod, Vrms, Vrmsd_1, Vrmsf);
+    //update the delayed terms.
+    Vrmsd_1 = Vrms;
+  }
+  VrmsSS = Vrms;
+  if (F == 0.0)
+  {
+    Serial.print("VrmsZero = ");
+  }
+  else
+  {
+    Serial.print("VrmsKnownLoad = ");
+  }
+  Serial.println(VrmsSS);
+}
+
+void calibrate_sensor()
+{
+  check_for_known_pressure(0.0);
+  Serial.println("Wait while the sensor is calibrated.");
+  record_ADC_output_known_pressure(0.0);
+  
+  check_for_known_pressure(knownLoad);
+  Serial.println("Wait while the sensor is calibrated.");
+  record_ADC_output_known_pressure(knownLoad);
+  
+  Serial.println("Done. Enter the values for knownLoad, sensorSurfaceArea, VrmsZero, and VrmsKnownLoad into StreamPressureData.ino");
+}
+
+void setup() {
+  Serial.begin(9600); // Set Baud rate.
+  int PWMPin = 9;
+  analogWrite(PWMPin, 127); //50% Duty Cycle.
+  Vrmsd_1 = calculate_Vrms();
+  Serial.println("Make sure you have read the instructions at the top of the source code.");
+  calibrate_sensor();
+}
+
+void loop()
+{
+
+}
---
+++ StreamPressureData.ino
@@ -0,0 +1,121 @@
+/*
+Max Tree
+XO-NANO
+1/26/22
+
+Before running this code, calibrate your sensor with CalibrateXOnanoPressureSensor.ino.
+The calibration script will give you the numbers you need for VrmsKnownLoad, knownLoad,
+and VrmsZero.
+
+This code reads pressure sensor data output, calculates  the root-mean-square
+of the data, filters the rms voltages, and then the voltage is converted to
+pressure and sent from the Arduino Uno by UART. Use the Serial Plotter or
+Serial Monitor tools to view the live stream data.
+
+An extra feature of this code is an LED setup. Set the lowPressureRMSThresh
+and the highPressureRMSThresh to determine the pressure at which each LED
+will light.
+
+This code is designed for an Arduino Uno.
+
+For the connection diagram between an Arduino Uno and an XO-NANO pressure
+sensor, see https://github.com/XOnanoSmartfoam/Dev-Kit-Resources.
+*/
+
+/****** User Inputs Section ******/
+/* Arduino Settings */
+int sensorPin = A0;
+int lowPressureLED = 3;
+int highPressureLED = 4;
+//for the PWM pin, see setup() function.
+/* XO-NANO Pressure Sensor Settings */
+float VrmsSampleLimit = 500;
+int lowPressureRMSThresh = 600;
+int highPressureRMSThresh = 575;
+float cutoffFreq = 1.0;  //Hz.
+float sampPeriod = 0.093;  //s. This number is from the timestamp of the Arduino IDE Serial Monitor tool.
+//From Calibration
+float sensorSurfaceArea = 1.0; //square inches
+//float VrmsKnownLoad = 268.04; //2x2 sensor with scuff
+//float VrmsKnownLoad = 519.99; //2x2 sensor, no scuff
+float VrmsKnownLoad = 534.40; //1x1 sensor
+float knownLoad = 6.12; //lb
+//float VrmsZero = 549.04; //2x2 sensor with scuff
+//float VrmsZero = 565.15; //2x2 sensor, no scuff
+float VrmsZero = 612.42; //1x1 sensor
+/****** End User Inputs Section ******/
+
+
+/* Initialize Variables */
+float sensorValue = 0;
+float Vrms = 0;
+float Vrmsd_1 = 0; // "d_1 stands for delayed 1.
+float Vrmsf = 500;   // "f" stands for filtered. Start at high value to reduce filter warm up time.
+float pressure = 0; //psi.
+float m = (knownLoad/sensorSurfaceArea)/(VrmsKnownLoad-VrmsZero); // dPressure/dVrms
+
+
+float calculate_Vrms()
+{
+  float V = 0;
+  for (int i = 0; i < VrmsSampleLimit; i++)
+  {
+    sensorValue =  analogRead(sensorPin);
+    V = V + sensorValue*sensorValue;    
+  }
+
+  V = V/VrmsSampleLimit;
+  V = sqrt(V);
+
+  return V;
+}
+
+void LED_check(float Vrms)
+{
+  if (Vrms < highPressureRMSThresh)
+  {
+    digitalWrite(highPressureLED, HIGH);
+    digitalWrite(lowPressureLED, LOW);
+  }
+  else if (Vrms < lowPressureRMSThresh)
+  {
+    digitalWrite(highPressureLED, LOW);
+    digitalWrite(lowPressureLED, HIGH);
+  }
+  else
+  {
+    digitalWrite(highPressureLED, LOW);
+    digitalWrite(lowPressureLED, LOW);
+  }
+}
+
+float lpf(float fc, float Ts, float y, float yd_1, float yfd_1) // fc is cutoff freq (Hz), Ts is sampling period (s).
+{                                                               // y is raw data and yf is filtered data and the d terms are delayed terms.
+  return (fc*y + yfd_1/Ts)/(1/Ts+fc);                           //derived by converting lpf lapace domain into time domain and assuming yf_dot = (yf-yfd_1)/Ts.
+}
+
+float convert_to_pressure(float V)
+{
+  return ((V-VrmsZero)*m);
+}
+
+
+void setup() {
+  Serial.begin(9600); // Set Baud rate.
+  int PWMPin = 9;
+  analogWrite(PWMPin, 127); //50% Duty Cycle.
+  pinMode(lowPressureLED, OUTPUT);
+  pinMode(highPressureLED, OUTPUT);
+  Vrmsd_1 = calculate_Vrms();
+}
+
+void loop()
+{
+  Vrms = calculate_Vrms();
+  Vrmsf = lpf(cutoffFreq, sampPeriod, Vrms, Vrmsd_1, Vrmsf);
+  pressure = convert_to_pressure(Vrmsf);
+  Serial.println(pressure);
+  LED_check(Vrmsf);
+  //update the delayed terms.
+  Vrmsd_1 = Vrms;
+}
