5eebdb7352643642b9c35623c82f6d733e771d28
XO-NANO Smartfoam <jake.merrell@xonano.com>
2022-09-06 11:22:17 -0600

    Update XO_Balance_Calibration.ino
    --Max Tree

--- XO_Balance/XO_Balance_Calibration/XO_Balance_Calibration.ino
+++ XO_Balance/XO_Balance_Calibration/XO_Balance_Calibration.ino
@@ -1,14 +1,11 @@
 /***********************************************************************************
-  Max Tree
   XO-NANO Smartfoam
-  March 22, 2022
+  Max Tree
+  Sept 6, 2022
   
-  This program uses an ESP32 Feather to read in voltages for eight different XO_NANO
+  This program uses an ESP32 Feather to read in voltages for up to twelve different XO_NANO
   pressure sensors, calculate each root-mean-square voltage (Vrms), and then notify
-  a service with a 32byte package (conatining every Vrms) via BLE constantly. The
-  code is based on BLE_uart.ino example for ESP32 BLE Arduino. The ESP32 Feather
-  with this code is named on line of this code that is:
-  "BLEDevice::init("Pressure Sense");:
+  a client via UART.
 ************************************************************************************/
 
 #define numOfADCs 12
@@ -18,7 +15,7 @@ const int batteryLevelPin {A13};
 const int adcPin[12] {A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12}; // The order of the adcs listed here determines the order of the BLE Vrms outputs.
 
 // setting PWM properties
-const int ledPin {26}; // 32 corresponds to GPIO16. (PWM)
+const int ledPin {A0}; // 32 corresponds to GPIO16. (PWM)
 const int freq {1000};
 const int ledChannel {0};
 const int resolution {1};
@@ -30,7 +27,7 @@ float Vrms[12] = {0,0,0,0,
                   0,0,0,0};
 float batteryLevel = 0.0;
 float batteryLevelInitial = 0.0;
-float VrmsSampleLimit = 500;
+float VrmsSampleLimit = 250;
 float alpha = 0.2; // value between 0 (too filtered) and 1 (no filter). 
 float Vnew = 0.0;
 
@@ -71,10 +68,10 @@ void account_for_battery_level() //the change in voltage of the battery dictates
 
 void update_voltages()
 {
-  account_for_battery_level();
+//  account_for_battery_level();
   for(int i = 0; i < numOfADCs; i++)
   {
-    Vnew = Vrms_calculation(adcPin[i]) + deltaV;
+    Vnew = Vrms_calculation(adcPin[i]); // + deltaV;
     Vrms[i] = (Vnew*alpha) + ((1-alpha)*Vrms[i]); // active, low pass filter
     Serial.print(Vrms[i]);
     if (i < numOfADCs - 1)
@@ -94,7 +91,7 @@ void fill_voltage_history()
 {
   for (int j = 0; j < numOfADCs; j++)
   {
-    Vrms[j] = Vrms_calculation(adcPin[j]) + deltaV;
+    Vrms[j] = Vrms_calculation(adcPin[j]); // + deltaV;
   }
 }
 
@@ -110,14 +107,14 @@ void setup() {
 
   ledcWrite(ledChannel, 127);
 
-  calculate_battery_level();
-  batteryLevelInitial = batteryLevel;
+//  calculate_battery_level();
+//  batteryLevelInitial = batteryLevel;
 
   fill_voltage_history();
 }
 
 void loop()
 {      
-      calculate_battery_level();
+//      calculate_battery_level();
       update_voltages();
 }
